6cf3c5c4df1fd8c4c54d5de228a870c91cd7b7c1
Spiro Stameson <sstameso@caltech.edu>
2022-11-02 14:37:46 -0700

    Add code to split data into training and validation sets

--- rockley/main.py
+++ rockley/main.py
@@ -1,5 +1,6 @@
-from rockley.utils.data import hello
+from rockley.utils.data import load_data
 
 
 def main():
-    hello()
+    (X_train, y_train), (X_valid, y_valid) = load_data("data/train_regression.parquet")
+    print(X_train.shape, y_train.shape, X_valid.shape, y_valid.shape)
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,11 +1,8 @@
 import unittest
 
-from rockley.utils.data import test
-
 
 class TestData(unittest.TestCase):
     def test_example(self):
-        self.assertEqual(test(), 42)
         self.assertEqual(0, 0)
         self.assertAlmostEqual(0.001, 0, places=2)
         self.assertNotAlmostEqual(0.001, 0, places=3)
