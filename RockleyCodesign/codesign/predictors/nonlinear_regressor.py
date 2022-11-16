c7d19f19a66382afb538dc14ccd4a569caf90b0f
Andrew Ma <andrewma3125@gmail.com>
2022-11-15 19:12:20 -0800

    Update initialization

--- RockleyCodesign/codesign/predictors/nonlinear_regressor.py
+++ RockleyCodesign/codesign/predictors/nonlinear_regressor.py
@@ -3,18 +3,24 @@ import torch.nn as nn
 
 class NonlinearRegressor(nn.Module):
   def __init__(self, input_size):
+    def init_weights(m):
+      if isinstance(m, nn.Linear):
+        torch.nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+
     super(NonlinearRegressor, self).__init__()
     self.layer1 = nn.Sequential(
       nn.Linear(input_size, 20),
       nn.BatchNorm1d(20),
       nn.ReLU(),
     )
+    self.layer1.apply(init_weights)
 
     self.layer2 = nn.Sequential(
       nn.Linear(20, 10),
       nn.BatchNorm1d(10),
       nn.ReLU(),
     )
+    self.layer2.apply(init_weights)
 
     self.layer3 = nn.Sequential(
       nn.Linear(10, 1)
